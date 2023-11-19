963e19843898532726497545d8514b08218dc4cd
Murasko <git@murasko.de>
2023-11-19 18:28:17 +0100

    guessing game with small additions

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("DEBUG: The secret Number is: {secret_number}");
+
+    println!("Welcome to Murasko's Guessing Game!");
+
+    loop {
+        println!("Please input your guess:");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("You guessed too low! ({guess})"),
+            Ordering::Greater => println!("You guessed too high! ({guess})"),
+            Ordering::Equal => {
+                println!("You won!");
+                break;
+            }
+        }
+    }
+}
