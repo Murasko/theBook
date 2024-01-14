35f0f9f53465cdc07a202279118a08a7ae8c4255
Murasko <git@murasko.de>
2024-01-14 21:34:12 +0100

    partial ownership commit

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # theBook
+
 Here you can find projects and code snippets I created while working my way through the [rust book](https://doc.rust-lang.org/book/).
 
-# License
+## License
+
 All code is licensed under GNU General Public License v3.0.
 
-# Waypoint
+## Waypoint
+
 Currently at: https://doc.rust-lang.org/stable/book/ch04-00-understanding-ownership.html
--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,20 @@
 fn main() {
+    println!("Hello, world!");
+
+    let some_string = String::from("hello");
     
+    // This would work because some_string is still in scope and not yet moved/dropped
+    //let new_string = some_string;
+
+    takes_ownership(some_string);
+        
+    let new_string = some_string; // This will not work because some_string has been moved to the takes_ownership function and got dropped after the end of the function
+
+    let x = 5;
+
+    makes_copy(x);
+
+    println!("{}", x); // This will work because x is a primitive type and it's value is copied to the makes_copy function
 }
 
 // A scope is the range within a program for which an item is valid
@@ -15,3 +30,11 @@ fn string_type() {
     s.push_str(", world!"); // push_str() appends a literal to a String
     println!("{}", s); // This will print `hello, world!`
 }
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer); // This will print 5
+} // Here, some_integer goes out of scope. Nothing special happens
+
+fn takes_ownership(another_string: String) { // another_string comes into scope
+    println!("{}", another_string); // This will print `hello`
+} // Here, another_string goes out of scope and `drop` is called. The backing memory is freed
